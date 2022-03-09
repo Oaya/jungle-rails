2d14e1dab7f5d105fe3ee89a7f681b7b2f0c3b0d
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-03-09 19:21:38 +0000

    showing the warning when the cart is empty in the cart page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart
 
-  def enhanced_cart
+  def enhanced_cart 
     @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
   end
   helper_method :enhanced_cart
