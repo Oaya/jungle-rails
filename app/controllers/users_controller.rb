aeeab14fe991cc6451cc87eb558aa37f4203a7aa
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-03-10 19:52:02 +0000

    add userform

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      sessoion[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).parmit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+  
 end
