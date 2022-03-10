639cc169c0e51462f34abc1777808d98ed95442f
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-03-10 18:14:54 +0000

    add create new catefory functionality

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:create]
+    resources :categories
   end
 
   resources :about
