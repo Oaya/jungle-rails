d8318ce3a4973025642869635d4b5805cb6825e1
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-03-11 02:13:33 +0000

    add sales table and controller

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories
+    resources :sales, only: [:index, :new]
   end
 
   resources :about
