7a144a95619d4ec3b188dc84a6fa334b4a8331b8
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-03-15 22:12:04 +0000

    create product_details spec for test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -21,8 +21,8 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # ACT
     visit root_path
 
-    # # DEBUG
-    save_screenshot
+    # DEBUG
+    # save_screenshot
 
     # VERIFY
     expect(page).to have_css 'article.product',count: 10
